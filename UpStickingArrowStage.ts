0ed134d0fbe2cd50130033c86c7f96d4c9841a20
Anwesh43 <anweshthecool0@gmail.com>
2019-03-25 10:55:31 +0530

    adding scale methods

--- UpStickingArrowStage.ts
+++ UpStickingArrowStage.ts
@@ -9,6 +9,21 @@ const lines : number = 2
 const backColor : string = "#bdbdbd"
 const foreColor : string = "#673AB7"
 
+const scaleFactor : Function = (scale : number) : number => Math.floor(scale / scDiv)
+const maxScale : Function = (scale : number, i : number, n : number) : number => {
+    return Math.max(0, scale - i / n)
+}
+const divideScale : Function = (scale : number, i : number, n : number) : number => {
+    return Math.min(1 / n, maxScale(scale, i, n)) * n
+}
+const mirrorValue : Function = (scale : number, a : number, b : number) : number => {
+    const k : number = scaleFactor(scale)
+    return (1 - k) / a + k / b
+}
+const updateValue : Function = (scale : number, dir : number, a : number, b : number) : number => {
+    return mirrorValue(scale, a, b) * dir * scGap
+}
+
 class UpStickingArrowStage {
       canvas : HTMLCanvasElement = document.createElement('canvas')
       context : CanvasRenderingContext2D
