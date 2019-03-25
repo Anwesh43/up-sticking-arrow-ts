0067e369023b640d19c9816474f67539482142c6
Anwesh43 <anweshthecool0@gmail.com>
2019-03-25 11:05:50 +0530

    adding State class

--- UpStickingArrowStage.ts
+++ UpStickingArrowStage.ts
@@ -53,3 +53,25 @@ class UpStickingArrowStage {
           stage.handleTap()
       }
 }
+
+class State {
+    scale : number = 0
+    prevScale : number = 0
+    dir : number = 0
+    update(cb : Function) {
+        this.scale += updateValue(this.scale, this.dir, lines, 1)
+        if (Math.abs(this.scale - this.prevScale) > 1) {
+            this.scale = this.prevScale + this.dir
+            this.dir = 0
+            this.prevScale = this.scale
+            cb()
+        }
+    }
+
+    startUpdating(cb : Function) {
+        if (this.dir == 0) {
+            this.dir = 1 - 2 * this.prevScale
+            cb()
+        }
+    }
+}
